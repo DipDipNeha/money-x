51a185e1f498e2d187733daf891cdff15fe82c29
Dipak <kumardipak962@gmail.com>
2025-08-24 13:33:47 +0530

    First Commit

--- Spring-Boot-Money-X/src/main/java/com/pscs/moneyx/controller/AuthenticationController.java
+++ Spring-Boot-Money-X/src/main/java/com/pscs/moneyx/controller/AuthenticationController.java
@@ -13,6 +13,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,6 +32,7 @@ import com.pscs.moneyx.security.JWTGenerator;
 /**
  * 
  */
+@CrossOrigin
 @RestController
 @RequestMapping("/api/auth")
 public class AuthenticationController {
--- Spring-Boot-Money-X/src/main/java/com/pscs/moneyx/controller/CustomerBusinessController.java
+++ Spring-Boot-Money-X/src/main/java/com/pscs/moneyx/controller/CustomerBusinessController.java
@@ -5,8 +5,8 @@ package com.pscs.moneyx.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +20,7 @@ import com.pscs.moneyx.services.CustomerBusinessService;
 /**
  * 
  */
+@CrossOrigin
 @RestController
 @RequestMapping("/api/customer")
 public class CustomerBusinessController {
